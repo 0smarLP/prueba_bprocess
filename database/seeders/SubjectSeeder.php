e52d27077bb37cf9b551413eb1c21a140ed04caf
0smarLP <olopezi1400@alumno.ipn.mx>
2024-04-16 12:50:36 -0600

    Agregando seeder para la tabla 'subjects'

---
+++ database/seeders/SubjectSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use App\Models\Subject;
+
+
+class SubjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create();
+
+        for ($i = 0; $i < 5; $i++) {
+            Subject::create([
+                'codigo' => $faker->unique()->bothify('???###'),
+                'nombre' => $faker->word
+            ]);
+        }
+    }
+}
